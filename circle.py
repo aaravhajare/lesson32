8260c78068f73fd859212a223888ace1c87d4175
aaravhajare <hajareaarav66@gmail.com>
2025-05-12 12:13:30 +0530

    circle

---
+++ circle.py
@@ -0,0 +1,20 @@
+import pygame as p
+p.init()
+
+screen = p.display.set_mode((800,600))
+
+done = False
+
+p.draw.circle(screen, (0,255,0) , (200 , 200), 50)
+
+p.draw.circle(screen, (255,0,0) , (400 , 400) , 50 , 2)
+
+p.display.flip()
+
+
+while not done:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            done = True
+
+    
